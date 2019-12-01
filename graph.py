cf0f0788c26a391dd09bb6fd52885600e963c3c8
Adam Spannbauer <SpannbauerAdam@gmail.com>
2019-12-01 12:56:15 -0500

    document graph class

--- graph.py
+++ graph.py
@@ -1,4 +1,3 @@
-# TODO: finish documenting
 import numpy as np
 from node import Node
 
@@ -8,13 +7,49 @@ class Graph:
 
     >>> graph = Graph()
     >>> graph.nodes
+    {}
     """
     def __init__(self):
         self.nodes = {}
 
+    def __repr__(self):
+        return f'<Graph with {len(self.nodes)} nodes>'
+
     @property
     def _node_names(self):
         return [n.name for n in self.nodes.values()]
+
+    def _validate_graph(self, clean=False):
+        """Check that all edges reference existing nodes
+
+        :param clean: Should bad references be removed from the graph?
+        :return: list of dictionaries showing bad references.  For example,
+                 if node 'a' was referencing node 'c' which didn't exist the
+                 output would be: `[{'node': 'a', 'bad_ref': 'c'}]`
+
+        >>> graph = Graph()
+        >>> graph.add_connection('a', 'b', bi_directional=True)
+        >>> graph.nodes['a'].add_connections(names=['c'])
+        >>> graph.nodes['a'].edges
+        {'b': 0, 'c': 0}
+        >>> # noinspection PyProtectedMember
+        >>> invalid_references = graph._validate_graph(clean=True)
+        >>> invalid_references
+        [{'node': 'a', 'bad_ref': 'c'}]
+        >>> graph.nodes['a'].edges
+        {'b': 0}
+        """
+        bad_refs = []
+        for node_a in self._node_names:
+            for node_b in self.nodes[node_a].edges.keys():
+                if node_b not in self._node_names:
+                    bad_refs.append({'node': node_a, 'bad_ref': node_b})
+
+        if clean:
+            for bad_ref in bad_refs:
+                self.remove_connection(bad_ref['node'], bad_ref['bad_ref'])
+
+        return bad_refs
 
     def add_nodes(self, names, edges=None):
         """Add Node objects and edges to graph
@@ -46,9 +81,39 @@ class Graph:
             for n in names:
                 self.nodes[n] = Node(n)
 
-    def add_connection(self, name_a, name_b, bi_directional=False, weight=0, on_conflict=np.mean):
-        # TODO: finish examples in this doc
+    def remove_nodes(self, names, rm_edges=True):
+        """Delete nodes from the graph
+
+        :param names: list of node names to be deleted
+        :param rm_edges: remove edges that reference nodes to be deleted
+        :return: None; `nodes` attribute is modified
+
+        >>> graph = Graph()
+        >>> graph.add_nodes(['a', 'b', 'c'])
+        >>> graph
+        <Graph with 3 nodes>
+        >>> graph.remove_nodes(['a', 'b'])
+        >>> graph.nodes
+        {'c': <Node with 0 edges>}
+        >>> graph = Graph()
+        >>> graph.add_nodes(['a', 'b', 'c'], [{'b': 1}, {}, {}])
+        >>> graph.remove_nodes(['b', 'c'])
+        >>> graph.nodes['a'].edges
+        {}
+        >>> graph = Graph()
+        >>> graph.add_nodes(['a', 'b', 'c'], [{'b': 1}, {}, {}])
+        >>> graph.remove_nodes(['b', 'c'], rm_edges=False)
+        >>> graph.nodes['a'].edges
+        {'b': 1}
         """
+        for name in names:
+            self.nodes.pop(name, None)
+
+        if rm_edges:
+            self._validate_graph(clean=True)
+
+    def add_connection(self, name_a, name_b, bi_directional=False, weight=0, on_conflict=np.mean):
+        """Add edge(s) to the graph
 
         :param name_a: name of left hand node as str
         :param name_b: name of right hand node as str
@@ -65,11 +130,22 @@ class Graph:
         >>> graph.add_nodes(names=['a', 'b'])
         >>> graph.nodes
         {'a': <Node with 0 edges>, 'b': <Node with 0 edges>}
-        >>> graph.add_connection('a', 'b', bi_directional=False, weight=5)
+        >>> graph.add_connection('a', 'b', weight=5)
         >>> graph.nodes
         {'a': <Node with 1 edges>, 'b': <Node with 0 edges>}
         >>> graph.nodes['a'].edges
         {'b': 5}
+        >>> graph.add_connection('b', 'a', weight=5)
+        >>> graph.nodes
+        {'a': <Node with 1 edges>, 'b': <Node with 0 edges>}
+        >>> graph.nodes['b'].edges
+        {'a': 10}
+        >>> graph = Graph()
+        >>> graph.add_nodes(names=['a', 'b'])
+        >>> graph.add_connection('a', 'b', bi_directional=True, weight=5)
+        {'a': <Node with 1 edges>, 'b': <Node with 1 edges>}
+        >>> graph.nodes['b'].edges
+        {'a': 5}
         """
         if name_a not in self._node_names:
             self.add_nodes([name_a])
@@ -81,7 +157,55 @@ class Graph:
         if bi_directional:
             self.nodes[name_b].add_connections([name_a], weights=[weight], on_conflict=on_conflict)
 
+    def remove_connection(self, name_a, name_b, bi_directional=False):
+        """Remove edge(s) from the graph
+
+        :param name_a: name of left hand node as str
+        :param name_b: name of right hand node as str
+        :param bi_directional: should connections be removed from both nodes? if False only removed from left hand node
+        :return: None; `nodes` attribute is modified
+
+        >>> graph = Graph()
+        >>> graph.add_nodes(names=['a', 'b'], edges=[{'b': 10}, {'a': 5}])
+        >>> graph.nodes
+        {'a': <Node with 1 edges>, 'b': <Node with 1 edges>}
+        >>> graph.remove_connection('a', 'b')
+        >>> graph.nodes
+        {'a': <Node with 0 edges>, 'b': <Node with 1 edges>}
+        >>> graph.remove_connection('b', 'a')
+        >>> graph.nodes
+        {'a': <Node with 0 edges>, 'b': <Node with 0 edges>}
+        >>> graph.add_nodes(names=['a', 'b'], edges=[{'b': 10}, {'a': 5}])
+        >>> graph.nodes
+        {'a': <Node with 1 edges>, 'b': <Node with 1 edges>}
+        >>> graph.remove_connection('a', 'b', bi_directional=True)
+        >>> graph.nodes
+        {'a': <Node with 0 edges>, 'b': <Node with 0 edges>}
+        """
+        self.nodes[name_a].remove_connections([name_b])
+        if bi_directional:
+            self.nodes[name_b].remove_connections([name_a])
+
     def merge(self, graph, agg_fun=sum):
+        """Add the nodes & edges of another Graph object
+
+        :param graph: A Graph object to be merged into this one.
+        :param agg_fun: Function that accepts a list of numbers as input and outputs a single numeric value.
+                        Will be used to aggregate common edges between the 2 graphs.
+                        For example, in graph 1 there exists A--5-->B and in graph 2 there exists A --3--> B.
+                        If `sum` is the agg_fun, then the resulting merged graph will have A --8--> B.
+        :return: None; `nodes` attribute is modified
+
+        >>> graph_1 = Graph()
+        >>> graph_2 = Graph()
+        >>> graph_1.add_nodes(['a', 'b'], edges=[{'b': 5}, {}])
+        >>> graph_2.add_nodes(['a', 'c'], edges=[{'b': 3}, {}])
+        >>> graph_1.merge(graph_2)
+        >>> graph_1
+        <Graph with 3 nodes>
+        >>> graph_1.nodes['a'].edges['b']
+        8
+        """
         shared_keys = graph.nodes.keys() & self.nodes.keys()
         missing_keys = graph.nodes.keys() - self.nodes.keys()
 
@@ -94,20 +218,27 @@ class Graph:
             self.nodes[k] = graph.nodes[k]
 
     def set_all_weights(self, value):
+        """Set every edge weight in the graph to a certain value
+
+        :param value: value to set all edge weights to
+        :return: None; `nodes` attribute is modified
+
+        >>> graph = Graph()
+        >>> graph.add_nodes(['a', 'b', 'c'], [{'b': 0, 'c': 1}, {'a': 2}, {}])
+        >>> graph.nodes['a'].edges
+        {'b': 0, 'c': 1}
+        >>> graph.set_all_weights(-1)
+        >>> graph.nodes['a'].edges
+        {'b': -1, 'c': -1}
+        >>> graph.nodes['b'].edges
+        {'a': -1}
+        >>> graph.nodes['c'].edges
+        {}
+        """
         for name, node in self.nodes.items():
             edges = node.edges
             node.edges = {}
-            for k, _ in edges.items():
+            for k in edges.keys():
                 node.edges[k] = value
 
             self.nodes[name] = node
-
-
-if __name__ == '__main__':
-    graph_1 = Graph()
-    graph_2 = Graph()
-
-    graph_1.add_nodes(['a', 'b'], edges=[{'b': 2}, {}])
-    graph_2.add_nodes(['a', 'c'], edges=[{'b': 1}, {}])
-
-    graph_1.merge(graph_2)

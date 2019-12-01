58b9f82817c8230e702ef85c84e22340b106c630
Adam Spannbauer <SpannbauerAdam@gmail.com>
2019-12-01 13:54:59 -0500

    add cpu training to tictactoe class

--- tictactoe.py
+++ tictactoe.py
@@ -1,10 +1,11 @@
 import numpy as np
 import board_utils
+from graph import Graph
 
 
 class TicTacToe:
     """Play tic-tac-toe with 1s and 2s instead of Xs and Os"""
-    def __init__(self):
+    def __init__(self, cpu_knowledge=None):
         self.board = np.array([[0, 0, 0],
                                [0, 0, 0],
                                [0, 0, 0]])
@@ -12,6 +13,7 @@ class TicTacToe:
         self.winner = 0
         self.last_played_piece = None
         self.last_played_loc = None
+        self.cpu_knowledge = Graph() if cpu_knowledge is None else cpu_knowledge
 
     def __repr__(self):
         return self.board.__repr__()
@@ -33,66 +35,14 @@ class TicTacToe:
         """
         return board_utils.flatten_board(self)
 
-    def reset_game(self):
+    def reset_game(self, reset_cpu_knowledge=False):
         """Clear board"""
-        self.__init__()
-
-    def place_piece(self, value, position):
-        """Place a piece at given coords
-
-        :param value: value of piece to place (either 1 or 2)
-        :param position: coordinates to place piece as (x, y) on zero indexed 2d grid
-
-        >>> ttt = TicTacToe()
-        >>> ttt.place_piece(1, (0, 0))
-        >>> ttt.board
-        array([[1, 0, 0],
-               [0, 0, 0],
-               [0, 0, 0]])
-        >>> # ttt.place_piece(1, (0, 0))
-        IndexError: A piece is already placed at that position.
-        >>> ttt.game_is_over = True
-        >>> # ttt.place_piece(1, (0, 0))
-        ValueError: Game is over.
-        """
-        if self.game_is_over:
-            raise ValueError('Game is over.')
-
-        x, y = position
-        if self.board[y, x] == 0:
-            self.board[y, x] = value
-            self.last_played_loc = position
-            self.last_played_piece = value
+        if reset_cpu_knowledge:
+            cpu_knowledge = None
         else:
-            raise IndexError('A piece is already placed at that position.')
+            cpu_knowledge = self.cpu_knowledge
 
-        self.winner = self._game_over()
-
-    def place_random_piece(self, value):
-        """Place a piece on any open space on board
-
-        :param value: value of piece to randomly place
-
-        >>> np.random.seed(42)
-        >>> ttt = TicTacToe()
-        >>> ttt.place_random_piece(1)
-        >>> ttt.place_random_piece(2)
-        >>> ttt.place_random_piece(1)
-        >>> ttt.board
-        array([[0, 0, 0],
-               [2, 0, 1],
-               [1, 0, 0]])
-        """
-        open_spaces = np.where(self.board == 0)
-        n_open = len(open_spaces[0])
-
-        if not n_open:
-            self.game_is_over = True
-        else:
-            i = np.random.choice(n_open)
-            position = open_spaces[1][i], open_spaces[0][i]
-
-            self.place_piece(value, position)
+        self.__init__(cpu_knowledge=cpu_knowledge)
 
     @staticmethod
     def _three_in_a_row(values):
@@ -151,3 +101,178 @@ class TicTacToe:
             self.game_is_over = True
 
         return 0
+
+    def place_piece(self, value, position):
+        """Place a piece at given coords
+
+        :param value: value of piece to place (either 1 or 2)
+        :param position: coordinates to place piece as (x, y) on zero indexed 2d grid
+
+        >>> ttt = TicTacToe()
+        >>> ttt.place_piece(1, (0, 0))
+        >>> ttt.board
+        array([[1, 0, 0],
+               [0, 0, 0],
+               [0, 0, 0]])
+        >>> # ttt.place_piece(1, (0, 0))
+        IndexError: A piece is already placed at that position.
+        >>> ttt.game_is_over = True
+        >>> # ttt.place_piece(1, (0, 0))
+        ValueError: Game is over.
+        """
+        if self.game_is_over:
+            raise ValueError('Game is over.')
+
+        x, y = position
+        if self.board[y, x] == 0:
+            self.board[y, x] = value
+            self.last_played_loc = position
+            self.last_played_piece = value
+        else:
+            raise IndexError('A piece is already placed at that position.')
+
+        self.winner = self._game_over()
+
+    def place_random_piece(self, value):
+        """Place a piece on any open space on board
+
+        :param value: value of piece to randomly place
+
+        >>> np.random.seed(42)
+        >>> ttt = TicTacToe()
+        >>> ttt.place_random_piece(1)
+        >>> ttt.place_random_piece(2)
+        >>> ttt.place_random_piece(1)
+        >>> ttt.board
+        array([[0, 0, 0],
+               [2, 0, 1],
+               [1, 0, 0]])
+        """
+        open_spaces = np.where(self.board == 0)
+        n_open = len(open_spaces[0])
+
+        if not n_open:
+            self.game_is_over = True
+        else:
+            i = np.random.choice(n_open)
+            position = open_spaces[1][i], open_spaces[0][i]
+
+            self.place_piece(value, position)
+
+    def cpu_place_piece(self, value):
+        """Have a CPU player place a piece
+
+        If ai has not been trained then this is equivalent to TicTacToe.place_random_piece().
+        Train with TicTacToe.train_cpu().
+
+        :param value: value of piece for CPU to place
+
+        >>> np.random.seed(42)
+        >>> ttt = TicTacToe()
+        >>> ttt.place_piece(1, (0, 0))
+        >>> ttt.cpu_place_piece(2)
+        >>> ttt.board
+        array([[1, 0, 0],
+               [0, 0, 0],
+               [0, 2, 0]])
+        """
+        current_board = self.flat_board
+        fp_board = board_utils.first_person_board(current_board, value)
+        sp_board = board_utils.second_person_board(current_board, value)
+        try:
+            edges = self.cpu_knowledge.nodes[sp_board].edges
+        # If move never seen before in knowledge
+        except KeyError:
+            self.place_random_piece(value=value)
+            return
+
+        # Remove negatively weighted moves
+        edges = {k: v for k, v in edges.items() if v > 0}
+        best_moves = sorted(edges, key=edges.get)
+        for move in best_moves:
+            position = board_utils.board_diff(fp_board, move)
+            try:
+                self.place_piece(value=value, position=position)
+                return
+            # If desired move isn't an open space
+            except IndexError:
+                continue
+
+        # If no known moves available
+        self.place_random_piece(value=value)
+
+    def train_cpu(self, n_rounds=5000, random_move_percent=0.25):
+        """Train CPU AI to play against
+
+        :param n_rounds: Number of rounds for computer to play itself
+        :param random_move_percent: Chance for CPU to make random choice rather than best known choice
+        :return: None; cpu_knowledge attribute will be modified
+
+        >>> ttt = TicTacToe()
+        >>> ttt.cpu_knowledge
+        >>> ttt.train_cpu()
+        >>> ttt.cpu_knowledge
+        """
+        # Store moves per player in first person.
+        # Will be added to self.cpu_knowledge after each training game and reset
+        game_knowledge = [Graph(), Graph()]
+
+        # Player 1 will start
+        player = 1
+        count = 0
+        while count <= n_rounds:
+            prev_move = self.flat_board
+            # Place piece
+            # Randomly decide to ignore knowledge and place randomly
+            if np.random.random() <= random_move_percent:
+                self.place_random_piece(value=player)
+            else:
+                self.cpu_place_piece(player)
+            current_move = self.flat_board
+
+            # Store move for player of interest
+            # Example: game_knowledge[0].add_nodes(names=['000000000'], edges=[{'000000001': 0}])
+            game_knowledge[player - 1].add_nodes(
+                [board_utils.second_person_board(prev_move, player)],
+                [{board_utils.first_person_board(current_move, player): 0}]
+            )
+
+            # Change to other player's turn
+            player = 1 if player == 2 else 2
+
+            # Consolidate knowledge after round
+            if self.game_is_over:
+                count += 1
+                if self.winner is not 0:  # 0 means a tie
+                    winning_ind = self.winner - 1
+                    winning_moves = game_knowledge[winning_ind]
+                    losing_moves = game_knowledge[int(not winning_ind)]
+
+                    winning_moves.set_all_weights(5)
+                    losing_moves.set_all_weights(-5)
+
+                    self.cpu_knowledge.merge(winning_moves)
+                    self.cpu_knowledge.merge(losing_moves)
+                else:
+                    tie_moves_1 = game_knowledge[0]
+                    tie_moves_2 = game_knowledge[1]
+
+                    tie_moves_1.set_all_weights(0)
+                    tie_moves_2.set_all_weights(0)
+
+                    self.cpu_knowledge.merge(tie_moves_1)
+                    self.cpu_knowledge.merge(tie_moves_2)
+
+                # Reset for next round
+                self.reset_game()
+                game_knowledge = [Graph(), Graph()]
+                player = 1
+
+        self.reset_game()
+
+
+if __name__ == '__main__':
+    ttt = TicTacToe()
+    print(ttt.cpu_knowledge)
+    ttt.train_cpu(100)
+    print(ttt.cpu_knowledge)
